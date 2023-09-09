f86d295ab86d6af5c36c5623067139825f4c16aa
kunal-dey <kunaldey.iitdhn@gmail.com>
2023-09-09 10:58:56 +0530

    added models for delivery cost and intraday cost

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from logging import Logger
+
 from quart import Quart, request
 from quart_cors import cors
 from kiteconnect.exceptions import InputException
@@ -5,10 +7,13 @@ from kiteconnect.exceptions import InputException
 from constants.global_contexts import set_access_token
 from services import background_task
 from constants.global_contexts import kite_context
+from utils.logger import get_logger
 
 app = Quart(__name__)
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app = cors(app, allow_origin="*")
+
+logger: Logger = get_logger(__name__)
 
 
 @app.get("/")
@@ -26,8 +31,10 @@ async def set_token_request():
     route to set the access token which is received after login zerodha using starter app
     :return:
     """
+    global logger
     try:
         set_access_token(request.args["token"])
+        logger.info("TOKEN HAS BEEN SET")
         return {"message": "Token set"}
     except:
         return {"message": "there is an error"}
@@ -36,7 +43,7 @@ async def set_token_request():
 @app.get("/start")
 async def start_process():
     """
-
+    route checks whether login has been done and then starts the background task
     :return:
     """
     try:
@@ -56,8 +63,10 @@ async def stop_background_tasks():
         On being deployed if we need to manually stop the background task then
         this route is used
     """
+    global logger
     for task in app.background_tasks:
         task.cancel()
+    logger.info("STOPPED ALL BACKGROUND SERVICES")
     return {"message": "All task cancelled"}
 
 
